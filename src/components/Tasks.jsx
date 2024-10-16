15b409838a534c5a618ee1f3ae2eca09d78f4038
Jan Nahodil <nahodilhonza@email.cz>
2024-10-16 21:39:12 +0200

    Update Tasks.jsx

--- src/components/Tasks.jsx
+++ src/components/Tasks.jsx
@@ -1,11 +1,15 @@
 import NewTask from "./NewTask";
 
+//Tasks component to display a list of tasks and allow adding/removing tasks
 export default function Tasks({ tasks, onAdd, onDel }) {
   return (
     <section>
       <h2 className="text-2xl font-bold text-stone-700 mb-4">Tasks</h2>
+       {/* Render the NewTask component to add new tasks */}
       <NewTask onAdd={onAdd} />
+      {/* If no task exist diplay message to the user */}
       {tasks.length === 0 && <p className="text-stone-800 my-4">No tasks</p>}
+      {/* If there are tasks, than display tham */}
       {tasks.length > 0 && (
         <ul className="p-4 mt-8 rounded-md bg-stone-100">
           {tasks.map((tas) => (
