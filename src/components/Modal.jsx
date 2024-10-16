97f44a123cf929d6b711d625ee4edfc684539883
Jan Nahodil <nahodilhonza@email.cz>
2024-10-16 21:17:10 +0200

    Update Modal.jsx

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,16 +1,21 @@
 import { createPortal } from "react-dom";
 import { forwardRef, useImperativeHandle, useRef } from "react";
 
+//Modal component using forwardRef to allow the parent component to control the modal
 const Modal = forwardRef(function Modal({ children }, ref) {
-  const dial = useRef();
+  const dial = useRef(); //Used to reference to the <dialog> 
+  
+  //open() method to parent component via ref using useImperativeHandle
   useImperativeHandle(ref, () => {
     return {
       open() {
+        //Call showModal() on the <dialog> element to open the modal
         dial.current.showModal();
       },
     };
   });
 
+  //Using createPortal to render the modal into a different part of the DOM
   return createPortal(
     <dialog
       ref={dial}
@@ -27,4 +32,4 @@ const Modal = forwardRef(function Modal({ children }, ref) {
   );
 });
 
-export default Modal;
+export default Modal; //Export the Modal as default export
